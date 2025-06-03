8d86e600ab99bd8643aac23175c690c78a2f9c31
silpiAkter <silpiakter461@gmail.com>
2025-06-03 15:09:52 +0600

    added comment the all console

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function countVowels(str) {
   return count;
 }
 
-console.log(countVowels("Your Name")); 
-console.log(countVowels("I Love My Country")); 
+// console.log(countVowels("Your Name")); 
+// console.log(countVowels("I Love My Country")); 
 
 
 // 2. array sorted in ascending order
@@ -26,36 +26,36 @@ function sortNumbers(arr) {
   return arr.sort((a, b) => a - b);
 }
 
-console.log(sortNumbers([4, 1, 8, 3]));
+// console.log(sortNumbers([4, 1, 8, 3]));
 
 // 3. reverseString
 function reverseString(str) {
   return str.split('').reverse().join('');
 }
 
-console.log(reverseString("hello")); 
+// console.log(reverseString("hello")); 
 
 // 4. returns the last element.
 function getLastElement(arr) {
   return arr[arr.length - 1];
 }
 
-console.log(getLastElement([10, 20, 30, 40]));
+// console.log(getLastElement([10, 20, 30, 40]));
 
 // 5. array containing elements
 function mergeArrays(arr1, arr2) {
   return arr1.concat(arr2);
 }
 
-console.log(mergeArrays([1, 2], [3, 4]));
+// console.log(mergeArrays([1, 2], [3, 4]));
 
 // 6. space character
 function hasSpace(str) {
   return str.includes(' ');
 }
 
-console.log(hasSpace("Ostad Limited"));  
-console.log(hasSpace("Ostad"));  
+// console.log(hasSpace("Ostad Limited"));  
+// console.log(hasSpace("Ostad"));  
 
 
 // 7. Empty String
@@ -63,13 +63,13 @@ function isEmptyString(str) {
   return str === '';
 }
 
-console.log(isEmptyString(""));  
-console.log(isEmptyString("Hello")); 
+// console.log(isEmptyString(""));  
+// console.log(isEmptyString("Hello")); 
 
 // 8. Remove Negative Numbers
 function removeNegativeNumbers(arr) {
   return arr.filter(num => num >= 0);
 }
 
-console.log(removeNegativeNumbers([-5, 2, -1, 6, 0]));
+// console.log(removeNegativeNumbers([-5, 2, -1, 6, 0]));
 
